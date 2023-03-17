15bde4dab7a3f8d4280757641fa17682e859eeaf
jtaavola <jesse@taavola.dev>
2023-03-16 23:48:16 -0500

    add simple twilio webhook endpoint

--- api/index.js
+++ api/index.js
@@ -1,8 +1,25 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const twilio = require("twilio");
+const app = express();
 
-app.get('/api/hello', (req, res) => {
-  res.send('Hello World!');
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const toNumber = process.env.TWILIO_TO_NUMBER;
+const fromNumber = process.env.TWILIO_FROM_NUMBER;
+const client = twilio(accountSid, authToken);
+
+app.post("/api/reply", async (req, res) => {
+  try {
+    const message = await client.messages.create({
+      body: "Done",
+      from: fromNumber,
+      to: toNumber,
+    });
+    console.log("Message sent:", message.sid);
+    res.send();
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = app;
